d85330f3e77ff5f4f98ac6812ea3968271fd8113
Gabriel <gabrieljaeger1@gmail.com>
2023-09-12 15:25:16 +0800

    more functions

--- src/main/java/com/example/FunctionsAssignment.java
+++ src/main/java/com/example/FunctionsAssignment.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class FunctionsAssignment {
 
-    // TODO: Implement a method that accepts two integers as parameters and returns their sum
+    // TODO: Implement a method that accepts two integers as parameters and returns
+    // their sum
     public int sum(int a, int b) {
         return a+b; // Replace with your implementation
     }
@@ -12,13 +13,58 @@ public class FunctionsAssignment {
         return ""; // Replace with your implementation
     }
 
-    // TODO: Implement a method that accepts an array of integers and returns the maximum value
+    // TODO: Implement a method to return a greeting message of the format: "Hello, John!"
+    public String greeting(String name) {
+        return "Hello, " + name + "!";
+        //return ""; // Replace with your implementation
+    }
+
+    // TODO: Implement a method that accepts an array of integers and returns the
+    // maximum value
     public int findMax(int[] arr) {
         return 0; // Replace with your implementation
     }
 
-    // TODO: Implement a method that accepts a string and returns true if it is a palindrome, false otherwise
+    // TODO: Implement a method that accepts a string and returns true if it is a
+    // palindrome, false otherwise
     public boolean isPalindrome(String str) {
         return false; // Replace with your implementation
     }
+
+    // TODO: Implement a method to find the longest word in a string
+    public String findLongestWord(String str) {
+        return ""; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to calculate the average of an array of numbers
+    public double calculateAverage(int[] nums) {
+        return 0; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to check if a number is prime
+    public boolean isPrime(int num) {
+        return false; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to calculate the power of a number
+    public double calculatePower(double base, double exponent) {
+        return 0; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to count the occurrences of a character in a string
+    public int countCharacterOccurrences(String str, char ch) {
+        return 0; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to find the largest number in an array
+    public int findLargestNumber(int[] nums) {
+        return 0; // Replace with your implementation
+    }
+
+    // TODO: Implement a method to calculate the sum of an array
+    public int calculateArraySum(int[] nums) {
+        return 0; // Replace with your implementation
+    }
+
+    
 }
--- src/test/java/com/example/FunctionsAssignmentTests.java
+++ src/test/java/com/example/FunctionsAssignmentTests.java
@@ -18,6 +18,11 @@ class FunctionsAssignmentTests {
     }
 
     @Test
+    void testGreeting() {
+        assertEquals("Hello, John!", functionsAssignment.greeting("John"));
+    }
+
+    @Test
     void testFindMaxMethod() {
         assertEquals(5, functionsAssignment.findMax(new int[]{1, 3, 5, 2, 4}));
     }
@@ -26,5 +31,41 @@ class FunctionsAssignmentTests {
     void testIsPalindromeMethod() {
         assertTrue(functionsAssignment.isPalindrome("madam"));
         assertFalse(functionsAssignment.isPalindrome("hello"));
+    }
+
+    @Test
+    void testFindLongestWord() {
+        assertEquals("jumped", functionsAssignment.findLongestWord("The quick brown fox jumped over the lazy dog"));
+    }
+
+    @Test
+    void testCalculateAverage() {
+        assertEquals(3.0, functionsAssignment.calculateAverage(new int[]{1, 2, 3, 4, 5}), 0.001);
+    }
+
+    @Test
+    void testIsPrime() {
+        assertTrue(functionsAssignment.isPrime(7));
+        assertFalse(functionsAssignment.isPrime(4));
+    }
+
+    @Test
+    void testCalculatePower() {
+        assertEquals(8.0, functionsAssignment.calculatePower(2, 3), 0.001);
+    }
+
+    @Test
+    void testCountCharacterOccurrences() {
+        assertEquals(3, functionsAssignment.countCharacterOccurrences("hello hello hello", 'l'));
+    }
+
+    @Test
+    void testFindLargestNumber() {
+        assertEquals(5, functionsAssignment.findLargestNumber(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    void testCalculateArraySum() {
+        assertEquals(15, functionsAssignment.calculateArraySum(new int[]{1, 2, 3, 4, 5}));
     }
 }
